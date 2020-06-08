f8a3a550f843e8a29e6676c74cff6e7a64f80b02
cralr <rafamcruza10@gmail.com>
2020-06-08 21:17:50 +0200

    Ejercicios Puzzle acabado

---
+++ PuzzleInfantil/app/src/main/java/com/example/puzzleinfantil/Win.java
@@ -0,0 +1,13 @@
+package com.example.puzzleinfantil;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Win extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_victory);
+    }
+}
